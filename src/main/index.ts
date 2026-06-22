2865cbefceedfd87414e3db66e15e0ee9676d411
NetworkMD <robertsond1987@gmail.com>
2026-06-22 02:36:51 +0100

    feat: scaffold electron-vite app that launches an empty window

---
+++ src/main/index.ts
@@ -0,0 +1,34 @@
+import { app, BrowserWindow } from 'electron'
+import { join } from 'node:path'
+
+function createWindow(): BrowserWindow {
+  const win = new BrowserWindow({
+    width: 1100,
+    height: 720,
+    title: 'Notes & Codes',
+    backgroundColor: '#1e1e1e',
+    webPreferences: {
+      preload: join(__dirname, '../preload/index.js'),
+      contextIsolation: true,
+      nodeIntegration: false,
+      sandbox: true
+    }
+  })
+  if (process.env.ELECTRON_RENDERER_URL) {
+    win.loadURL(process.env.ELECTRON_RENDERER_URL)
+  } else {
+    win.loadFile(join(__dirname, '../renderer/index.html'))
+  }
+  return win
+}
+
+app.whenReady().then(() => {
+  createWindow()
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
+})
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit()
+})
